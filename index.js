e4fceda66c0432ac910d5e699e8cfbac0853a5da
Shahruk Kabir <shahrukkabir01@gmail.com>
2025-04-09 20:22:34 +0600

    Setup MongoDB client connection

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-const jwt = require('jsonwebtoken');
-const cookieParser = require('cookie-parser');
-const app = express();
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
@@ -16,3 +17,30 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(cookieParser());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bjzga.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    console.log("MongoDB connected!");
+  } finally {
+    // close logic if needed
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("All WhereIsIt");
+});
+
+app.listen(port, () => {
+  console.log(`WhereIsIt server is running on port: ${port}`);
+});
