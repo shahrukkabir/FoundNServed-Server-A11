d810b6667b0f2ba59f323861ce6a0cb105442760
Shahruk Kabir <shahrukkabir01@gmail.com>
2025-04-09 20:33:21 +0600

    Add JWT login and logout routes

--- index.js
+++ index.js
@@ -42,6 +42,27 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: '12h' });
+      res
+        .cookie('token', token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ message: 'Success' });
+    });
+    
+    app.post('/logout', async (req, res) => {
+      res
+        .clearCookie('token', {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ message: 'Success' });
+    });
     console.log("MongoDB connected!");
   } finally {}
 }
